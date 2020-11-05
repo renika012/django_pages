525c89982f0a26de12ba4697dae80e07ee21744a
renika012 <renika012@yahoo.com>
2020-11-05 17:54:00 -0600

    configuring for heroku

--- django_pages/urls.py
+++ django_pages/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')), # new
+    path('', include('pages/',('pages.urls')), # new
 ]
