ff591ad0719028bdee167f0308a630b4004865af
renika012 <renika012@yahoo.com>
2020-11-05 20:35:19 -0600

    configuring for heroku

--- django_pages/urls.py
+++ django_pages/urls.py
@@ -1,6 +1,7 @@
 # django_pages/urls.py
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, include # new
+from django.urls import path # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,14 @@
 # pages/views.py
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+def post_list(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'pages/post_list.html', {'posts': posts})
